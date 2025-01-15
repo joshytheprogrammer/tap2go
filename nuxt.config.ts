ea3079a34e81ca09b41fb636e7caa04138f726aa
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-01-16 00:54:54 +0100

    made changes: ''

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@vite-pwa/nuxt']
+  modules: ['@pinia/nuxt', '@vite-pwa/nuxt', '@nuxtjs/seo', 'nuxt-vuefire'],
+  vuefire: {
+    auth: {
+      enabled: true
+    },
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  },
 })
