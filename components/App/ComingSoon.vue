90db87d843f9e4501c65c44d9c0f6d38cfc84482
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-01-16 20:12:17 +0100

    made changes: ''

---
+++ components/App/ComingSoon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div class="text-center p-6 bg-white shadow-xl rounded-lg max-w-md w-full">
+      <h1 class="text-4xl font-bold text-gray-800 animate__animated animate__fadeIn">
+        Coming Soon!
+      </h1>
+      <p class="mt-4 text-lg text-gray-600 animate__animated animate__fadeIn animate__delay-1s">
+        We're working hard to bring you something amazing. Stay tuned!
+      </p>
+      <div class="mt-6">
+        <div class="inline-block animate__animated animate__bounce animate__delay-2s">
+          <svg class="w-12 h-12 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3M16 9l3 3-3 3" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
+</style>
