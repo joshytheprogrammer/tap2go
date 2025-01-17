b61ee0db6130f3ce43d6504d0023a7bc0aa8e4d9
joshytheprogrammer <jurassicjoshua@protonmail.com>
2025-01-17 13:29:39 +0100

    made changes: still improving the homepage

---
+++ components/Home/Recent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="space-y-4 px-4 py-4">
+    <!-- Transaction Item -->
+    <div class="flex items-center gap-4">
+      <!-- Icon -->
+      <div>
+        <img 
+          src="https://randomuser.me/api/portraits/men/1.jpg" 
+          alt="Transaction Icon" 
+          class="w-12 h-12 rounded-full"
+        />
+        
+        <!-- Details -->
+        <div class="flex flex-col">
+          <p class="text-gray-800 font-medium">Credit alert</p>
+          <p class="text-sm text-gray-500">3 Days Ago</p>
+        </div>
+      </div>
+      
+      <!-- Details -->
+      <p class="text-gray-800 font-medium">₦10,000.00</p>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ components/Home/Summary.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="px-4 py-8">
+    <h2 class="text-xl font-semibold pb-4">Last 30 Days</h2>
+    
+    <div class="flex justify-between gap-4 ">
+      <!-- Cash In -->
+      <div class="bg-primary-600 text-white rounded-lg w-full px-6 py-4 space-y-2 text-center">
+        <p class="text-sm">Cash In</p>
+        <p class="text-xl font-semibold">₦10,000.00</p>
+      </div>
+
+      <!-- Cash Out -->
+      <div class="bg-yellow-500 text-white rounded-lg w-full px-6 py-4 space-y-2 text-center">
+        <p class="text-sm">Cash Out</p>
+        <p class="text-xl font-semibold">₦4,999.25</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
